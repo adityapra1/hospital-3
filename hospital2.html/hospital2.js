801a4b4a659d60479e4efbbe73f3ed2a500a3c21
adityapra1 <164087454+adityapra1@users.noreply.github.com>
2024-06-09 08:33:29 +0530

    Add files via upload

--- hospital2.html/hospital2.js
+++ hospital2.html/hospital2.js
@@ -18,14 +18,18 @@ var swiper = new Swiper(".home", {
     },
   });
 
- $(document) .ready (function (){
-    $("#news-slider").owlCarousel({
-        item:3,
-        navigation:true,
-        navigationText:["",""],
-        autoPlay:true
-
-    });
-
- });
-  
+  var swiper = new Swiper(".",  {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });

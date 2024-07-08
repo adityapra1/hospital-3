eefa307b9d198316e5870d6ba1180470bb2b604f
adityapra1 <adityaprakash.shibu@gmail.com>
2024-07-09 00:00:33 +0530

    footer

--- public/script.js
+++ public/script.js
@@ -19,3 +19,20 @@ var swiper = new Swiper(".home", {
   });
 
   
+let valueDisplays =  document.querySelectorAll(".num");
+let interval = 1000;
+
+
+valueDisplays.forEach((valueDisplay) => { 
+  let startValue =1000;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue );
+  let counter = setInterval(function () {
+  startValue +=1;
+  valueDisplay.textContent = startValue;
+  if (startValue == endValue) {
+    clearInterval(counter);
+  }
+ }, duration);
+}); 
+
